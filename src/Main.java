1e1d340ecc6d103c51ac0481617a3eec3119255f
Maxx <maxx@aolc.ru>
2023-10-02 09:41:08 +0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        while (true){
+            playTheGame(getUserChoice(scanner), getCompChoice());
+        }
+    }
+    public static int getCompChoice(){
+        Random rand = new Random();
+        return rand.nextInt(3);
+    }
+    public static int getUserChoice(Scanner scanner){
+        System.out.println("Выбирай:");
+        System.out.println("1. Камень");
+        System.out.println("2. Ножницы");
+        System.out.println("3. Бумага");
+        String choice = scanner.nextLine();
+        return Integer.parseInt(choice) - 1;
+    }
+    public static void playTheGame(int userChoice, int compChoice){
+        String[] choice = {"Камень", "Ножницы", "Бумага"};
+        System.out.println("Ты: " + choice[userChoice] + userChoice + " Комп: " + choice[compChoice] +  compChoice);
+        if (userChoice - compChoice == 1 || userChoice - compChoice == -2){
+            System.out.println("Комп выиграл");
+        } else if (userChoice - compChoice == -1 || userChoice - compChoice == 2){
+            System.out.println("Ты выиграл");
+        } else {
+            System.out.println("Перекидывайте");
+        }
+        System.out.println(userChoice - compChoice);
+    }
+}
+
